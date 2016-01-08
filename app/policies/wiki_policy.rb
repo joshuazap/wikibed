e03a5a9136e233bae6104cece38e97c1742af0f5
joshuazap <joshuazap@gmail.com>
2016-01-08 05:30:29 +0000

    Add wiki policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,2 +1,18 @@
 class WikiPolicy < ApplicationPolicy
+  class Scope < Scope
+    attr_reader :user, :wiki
+
+    def initialize(user, wiki)
+      @user = user
+      @wiki = wiki
+    end
+
+    def resolve
+      scope
+    end
+
+    def destroy?
+      user.admin? || wiki.user_id == user.id
+    end
+  end
 end
