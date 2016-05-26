97b04ba49a03af0ef37a28e586980dc66414371b
Joshua Zapata <joshuazap@gmail.com>
2016-05-25 17:20:29 -0700

    Add back-to-top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,20 @@
 //= require bootstrap.min
 //= require turbolinks
 //= require_tree .
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
